4ce88ea62beb1129981f6540d7ed10d2e8a71a37
Gisele Macieira <giselefchaves@gmail.com>
2023-05-05 16:14:14 -0300

    finalização de alguns dados dinâmicos -Adição do dado dinâmico de nome de usuário -Adição do dado dinâmico de imagem de perfil -Adição de dado dinâmco de salvar post

--- src/User.js
+++ src/User.js
@@ -3,14 +3,26 @@ import React from 'react';
 export default function User(){
     const [user, setUser] = React.useState('beauty.summer');
     const [profileimg, setProfileimg] = React.useState('assets/img/summer-perfil.jpg');
+    function alterarNome(){
+        let novoNome = prompt('Qual o nome desejado?');
+        if(novoNome){
+            setUser(novoNome);
+        }
+    }
+    function alterarFoto(){
+        let novafoto = prompt('Insira o link da nova imagem');
+        if(novafoto){
+            setProfileimg(novafoto);
+        }
+    }
 
     return(
         <div class="usuario">
-          <img src={profileimg} alt="imagem de perfil"/>
+          <img src={profileimg} alt="imagem de perfil" onClick = {alterarFoto} />
           <div class="texto">
             <span>
               <strong>{user}</strong>
-              <ion-icon name="pencil"></ion-icon>
+              <ion-icon name="pencil" onClick = {alterarNome}></ion-icon>
             </span>
           </div>
         </div>
